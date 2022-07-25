3c61f3e937d46073b609e617476379870c11d768
Alex <me@djoh.xyz>
2022-07-26 00:22:56 +0300

    Update index.ts

--- src/server/index.ts
+++ src/server/index.ts
@@ -17,11 +17,10 @@ app.post("/webhook/navio", async (req, res) => {
     console.log("header", req.headers["navio-signature"]);
     console.log("hmac", hmacSHA256(config.navio_secrets[0], JSON.stringify(req.body)));
 
-    console.log("body", req.body);
-    console.log(typeof req.body);
-
     if (req.headers["navio-signature"] !== hmacSHA256(config.navio_secrets[0], JSON.stringify(req.body))) return;
     if ((req.body as any).type !== "job.delivered") return;
+
+    console.log(req.body);
 });
 
 app.get("*", async (req, res) => {
