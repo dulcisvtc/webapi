f32e618d2cdfda7107725b78d9c2abcc832e3f25
Vadym <me@djoh.xyz>
2022-12-11 13:18:27 +0200

    fix user database

--- src/database/user.ts
+++ src/database/user.ts
@@ -1,18 +1,17 @@
 import type { UserDocument } from "./models/User";
-import type { Snowflake } from "discord.js";
 import { User } from "./models/User";
 
-export function getUserDocument(userId: Snowflake): Promise<UserDocument> {
+export function getUserDocument(steamId: string): Promise<UserDocument> {
     return new Promise<UserDocument>((resolve) => {
-        void User.findOne({ userId }).then((userInDb) => {
-            const user = userInDb ?? new User({ userId });
+        void User.findOne({ steam_id: steamId }).then((userInDb) => {
+            const user = userInDb ?? new User({ steam_id: steamId });
 
             return resolve(user);
         });
     });
 };
 
-export async function resetUserDocument(userid: Snowflake): Promise<void> {
+export async function resetUserDocument(userid: string): Promise<void> {
     const user = await getUserDocument(userid);
     return void user.remove();
 };
