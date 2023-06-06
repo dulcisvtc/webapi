affccc40fd44ce9428d416ebff45281636cde2c3
Vadym <me@djoh.xyz>
2023-06-06 14:13:43 +0300

    fix tmp bigints

--- src/lib/http.ts
+++ src/lib/http.ts
@@ -1,12 +1,23 @@
 import axios, { AxiosError } from "axios";
 import { setTimeout as sleep } from "timers/promises";
+import JSONbigint from "json-bigint";
 
 const http = axios.create({
     maxRedirects: 0,
     timeout: 30_000
 });
 
-http.interceptors.response.use(undefined, async (error: AxiosError) => {
+http.interceptors.request.use((request) => {
+    request.transformResponse = [data => data];
+
+    return request;
+});
+
+http.interceptors.response.use((response) => {
+    response.data = JSONbigint.parse(response.data);
+
+    return response;
+}, async (error: AxiosError) => {
     const { config } = error;
 
     if (!config?.retry || error.response?.status === 404)
