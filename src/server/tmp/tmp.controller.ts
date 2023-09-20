46fadd51722c51888c4361278964a5c5fda02f05
Vadym <me@djoh.xyz>
2023-09-21 01:52:03 +0300

    remove unused endpoints

--- src/server/tmp/tmp.controller.ts
+++ src/server/tmp/tmp.controller.ts
@@ -1,15 +1,10 @@
 import { Controller, Get, Param, ParseIntPipe } from "@nestjs/common";
-import type { APICompanyMembers, APICompanyNews, APIGameEvent } from "@truckersmp_official/api-types/v2";
+import type { APICompanyMembers, APIGameEvent } from "@truckersmp_official/api-types/v2";
 import { TMPService } from "./tmp.service";
 
 @Controller("tmp")
 export class TMPController {
     constructor(private tmpService: TMPService) { };
-
-    @Get("news")
-    async findNews(): Promise<{ response: APICompanyNews }> {
-        return await this.tmpService.getNews();
-    };
 
     @Get("members")
     async findMembers(): Promise<{ response: APICompanyMembers }> {
--- src/server/tmp/tmp.service.ts
+++ src/server/tmp/tmp.service.ts
@@ -1,17 +1,11 @@
 import { CacheTTL } from "@nestjs/cache-manager";
 import { Injectable } from "@nestjs/common";
-import type { APICompanyMembers, APICompanyNews, APIGameEvent } from "@truckersmp_official/api-types/v2";
+import type { APICompanyMembers, APIGameEvent } from "@truckersmp_official/api-types/v2";
 import ms from "ms";
 import http from "../../lib/http";
 
 @Injectable()
 export class TMPService {
-    async getNews(): Promise<{ response: APICompanyNews }> {
-        const data = (await http.get<{ response: APICompanyNews }>("https://api.truckersmp.com/v2/vtc/55939/news")).data;
-
-        return data;
-    };
-
     async getMembers(): Promise<{ response: APICompanyMembers }> {
         const data = (await http.get<{ response: APICompanyMembers }>("https://api.truckersmp.com/v2/vtc/55939/members")).data;
 
