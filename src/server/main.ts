ff1df5be8ebe93b4888518781fc81d8a4ecfd46c
Vadym <me@djoh.xyz>
2023-10-17 23:58:30 +0300

    try dis

--- src/server/main.ts
+++ src/server/main.ts
@@ -3,6 +3,7 @@ import { NestFactory } from "@nestjs/core";
 import morgan from "morgan";
 import config from "../config";
 import { AppModule } from "./modules/app.module";
+import type { Request } from "express";
 
 export async function bootstrap() {
     const app = await NestFactory.create(AppModule, {
@@ -15,7 +16,8 @@ export async function bootstrap() {
     app.use("trust proxy", (ip: string) => {
         return ["::ffff:172.23.0.1"].includes(ip);
     });
-    app.use(morgan(":remote-addr [:date] \":method :url HTTP/:http-version\" :status :res[content-length] - :response-time ms"));
+    morgan.token("ip", (req: Request) => req.ips.length ? req.ips[0]! : req.ip);
+    app.use(morgan(":ip [:date] \":method :url HTTP/:http-version\" :status :res[content-length] - :response-time ms"));
 
     app.useGlobalPipes(new ValidationPipe());
 
