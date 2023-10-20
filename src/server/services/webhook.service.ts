692f7aded2cdec1b7b3bdd86a8931d29be6bfd02
Vadym <me@djoh.xyz>
2023-10-20 16:08:58 +0300

    bim bim bam bam

--- src/server/services/webhook.service.ts
+++ src/server/services/webhook.service.ts
@@ -31,41 +31,39 @@ export class WebhookService {
             req.headers["tracksim-signature"] === hmacSHA256(secret, rawBody)
         )) throw new ForbiddenException("Invalid signature");
 
-        const job = body.data.object;
-
-        const newJob: JobSchema = {
-            ts_job_id: job.id,
+        const job: JobSchema = {
+            ts_job_id: body.data.object.id,
             driver: {
-                id: job.driver.id,
-                steam_id: job.driver.steam_id,
-                username: job.driver.username
+                id: body.data.object.driver.id,
+                steam_id: body.data.object.driver.steam_id,
+                username: body.data.object.driver.username
             },
-            start_timestamp: new Date(job.start_time).getTime(),
-            stop_timestamp: new Date(job.stop_time).getTime(),
-            driven_distance: job.driven_distance,
-            fuel_used: job.fuel_used,
+            start_timestamp: new Date(body.data.object.start_time).getTime(),
+            stop_timestamp: new Date(body.data.object.stop_time).getTime(),
+            driven_distance: body.data.object.driven_distance,
+            fuel_used: body.data.object.fuel_used,
             cargo: {
-                name: job.cargo.name,
-                mass: job.cargo.mass,
-                damage: job.cargo.damage
+                name: body.data.object.cargo.name,
+                mass: body.data.object.cargo.mass,
+                damage: body.data.object.cargo.damage
             },
-            source_city: job.source_city.name,
-            source_company: job.source_company.name,
-            destination_city: job.destination_city.name,
-            destination_company: job.destination_company.name,
-            truck: `${job.truck.brand.name} ${job.truck.name}`,
-            average_speed: job.truck.average_speed * 3.6,
-            top_speed: job.truck.top_speed * 3.6
+            source_city: body.data.object.source_city.name,
+            source_company: body.data.object.source_company.name,
+            destination_city: body.data.object.destination_city.name,
+            destination_company: body.data.object.destination_company.name,
+            truck: `${body.data.object.truck.brand.name} ${body.data.object.truck.name}`,
+            average_speed: body.data.object.truck.average_speed * 3.6,
+            top_speed: body.data.object.truck.top_speed * 3.6
         };
 
         if (job.driven_distance < 1) return { message: "OK" };
 
-        if (await Jobs.exists({ ts_job_id: newJob.ts_job_id })) {
-            jobsLogger.warn(`Job ${newJob.ts_job_id} already exists in database.`);
+        if (await Jobs.exists({ ts_job_id: job.ts_job_id })) {
+            jobsLogger.warn(`Job ${job.ts_job_id} already exists in database.`);
             return { message: "OK" };
         };
 
-        const user = await getUserDocumentBySteamId(job.driver.steam_id);
+        const user = await getUserDocumentBySteamId(job.driver.steam_id!);
         if (job.fuel_used < 0) job.fuel_used = 0;
 
         await Jobs.create(job);
@@ -76,35 +74,35 @@ export class WebhookService {
         const embed = new EmbedBuilder()
             .setTitle("Job delivered!")
             .setURL("https://hub.dulcis.org/jobs")
-            .setDescription(`${escapeMarkdown(newJob.driver.username)} (<@${user.discord_id}>) | ${newJob.driven_distance.toFixed(2)}km`)
+            .setDescription(`${escapeMarkdown(job.driver.username)} (<@${user.discord_id}>) | ${job.driven_distance.toFixed(2)}km`)
             .addFields({
                 name: "Source",
-                value: `${newJob.source_company} (${newJob.source_city})`,
+                value: `${job.source_company} (${job.source_city})`,
                 inline: true
             }, {
                 name: "Destination",
-                value: `${newJob.destination_company} (${newJob.destination_city})`,
+                value: `${job.destination_company} (${job.destination_city})`,
                 inline: true
             }, {
                 name: "Cargo",
-                value: `${newJob.cargo.name} | ${(newJob.cargo.mass / 1000).toFixed(1)}T | ${Math.round(newJob.cargo.damage * 100)}% damage`,
+                value: `${job.cargo.name} | ${(job.cargo.mass / 1000).toFixed(1)}T | ${Math.round(job.cargo.damage * 100)}% damage`,
                 inline: true
             }, {
                 name: "Truck",
-                value: `${newJob.truck}`,
+                value: `${job.truck}`,
                 inline: true
             }, {
                 name: "Speed",
-                value: `max: ${newJob.top_speed.toFixed(1)}km/h | avg: ${newJob.average_speed.toFixed(1)}km/h`,
+                value: `max: ${job.top_speed.toFixed(1)}km/h | avg: ${job.average_speed.toFixed(1)}km/h`,
                 inline: true
             }, {
                 name: "Fuel used",
-                value: `${newJob.fuel_used.toFixed(1)}L`,
+                value: `${job.fuel_used.toFixed(1)}L`,
                 inline: true
             });
 
-        user.leaderboard.monthly_mileage += newJob.driven_distance;
-        user.leaderboard.alltime_mileage += newJob.driven_distance;
+        user.leaderboard.monthly_mileage += job.driven_distance;
+        user.leaderboard.alltime_mileage += job.driven_distance;
 
         await Promise.all([
             channel.send({ embeds: [embed] })
