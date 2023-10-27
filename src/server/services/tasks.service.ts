163ef41467e43f6d79dddc2e923dac5bfa426eca
Vadym <djoh@proxied.host>
2023-10-27 23:29:28 +0300

    dont need to do this anymore

--- src/server/services/tasks.service.ts
+++ src/server/services/tasks.service.ts
@@ -242,7 +242,7 @@ export class TasksService {
             platform_name: "Dulcis Logistics Driver's Hub",
             platform_username: driver.username,
             metadata: {
-              kms: Math.round(driver.leaderboard.alltime_mileage),
+              kms: driver.leaderboard.alltime_mileage,
               jobs: jobs,
             },
           },
