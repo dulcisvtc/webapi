13b33fca3c72101ebc0be4b91b4018aa998bc78c
Vadym <djoh@proxied.host>
2023-10-28 00:32:43 +0300

    hope this fixes it

--- src/server/modules/auth.module.ts
+++ src/server/modules/auth.module.ts
@@ -2,12 +2,13 @@ import { Module } from "@nestjs/common";
 import { AuthController } from "../controllers/auth.controller";
 import { PermissionsGuard } from "../guards/auth.guard";
 import { AuthService } from "../services/auth.service";
+import { UsersService } from "../services/users.service";
 import { UsersModule } from "./users.module";
 
 @Module({
   imports: [UsersModule],
   controllers: [AuthController],
   providers: [AuthService, PermissionsGuard],
-  exports: [AuthService, PermissionsGuard],
+  exports: [AuthService, PermissionsGuard, UsersService],
 })
 export class AuthModule {}
--- src/server/modules/webhook.module.ts
+++ src/server/modules/webhook.module.ts
@@ -1,11 +1,12 @@
 import { Module } from "@nestjs/common";
 import { WebhookController } from "../controllers/webhook.controller";
+import { UsersService } from "../services/users.service";
 import { WebhookService } from "../services/webhook.service";
 import { UsersModule } from "./users.module";
 
 @Module({
   imports: [UsersModule],
   controllers: [WebhookController],
-  providers: [WebhookService],
+  providers: [WebhookService, UsersService],
 })
 export class WebhookModule {}
