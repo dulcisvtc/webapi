870e7030e4f4d0267fc45a617a43b616a0dbe38a
Vadym <djoh@proxied.host>
2023-10-28 00:36:01 +0300

    aaaaaaa

--- src/server/modules/auth.module.ts
+++ src/server/modules/auth.module.ts
@@ -3,10 +3,8 @@ import { AuthController } from "../controllers/auth.controller";
 import { PermissionsGuard } from "../guards/auth.guard";
 import { AuthService } from "../services/auth.service";
 import { UsersService } from "../services/users.service";
-import { UsersModule } from "./users.module";
 
 @Module({
-  imports: [UsersModule],
   controllers: [AuthController],
   providers: [AuthService, PermissionsGuard],
   exports: [AuthService, PermissionsGuard, UsersService],
--- src/server/modules/webhook.module.ts
+++ src/server/modules/webhook.module.ts
@@ -2,10 +2,8 @@ import { Module } from "@nestjs/common";
 import { WebhookController } from "../controllers/webhook.controller";
 import { UsersService } from "../services/users.service";
 import { WebhookService } from "../services/webhook.service";
-import { UsersModule } from "./users.module";
 
 @Module({
-  imports: [UsersModule],
   controllers: [WebhookController],
   providers: [WebhookService, UsersService],
 })
