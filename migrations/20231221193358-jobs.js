319f303abdebe1b6b6426fa03fc02f555d63c159
djoh <djoh@proxied.host>
2023-12-21 23:19:49 +0200

    Jobs schema

---
+++ migrations/20231221193358-jobs.js
@@ -0,0 +1,26 @@
+module.exports = {
+  async up(db, client) {
+    const Jobs = db.collection("jobs");
+
+    const jobs = await Jobs.find({});
+
+    for (const job of jobs) {
+      job.source = {};
+      if (job.job_id) {
+        job.source.id = job.job_id;
+        job.source.name = "navio";
+
+        delete job.job_id;
+      } else if (job.ts_job_id) {
+        job.source.id = job.ts_job_id;
+        job.source.name = "tracksim";
+
+        delete job.ts_job_id;
+      }
+
+      await job.save();
+    }
+  },
+
+  async down(db, client) {},
+};
--- src/database/models/Jobs.ts
+++ src/database/models/Jobs.ts
@@ -1,5 +1,52 @@
-import { model, Schema } from "mongoose";
-import type { JobSchema } from "../../../types";
+import { AutoIncrementID } from "@typegoose/auto-increment";
+import { DocumentType, getModelForClass, index, modelOptions, plugin, prop } from "@typegoose/typegoose";
 
-const JobSchema = new Schema<JobSchema>({}, { strict: false });
-export const Jobs = model<JobSchema>("Jobs", JobSchema);
+@modelOptions({ schemaOptions: { _id: false, versionKey: false } })
+class DriverSchema {
+  @prop({ type: Number }) id!: number | null;
+  @prop({ type: String }) steam_id!: string | null;
+  @prop({ type: String }) username!: string;
+}
+
+@modelOptions({ schemaOptions: { _id: false, versionKey: false } })
+class SourceSchema {
+  @prop({ type: Number }) id!: number | null;
+  @prop({ type: String, enum: ["navio", "tracksim"] }) name!: string | null;
+}
+
+@modelOptions({ schemaOptions: { _id: false, versionKey: false } })
+class CargoSchema {
+  @prop({ type: String }) name!: string;
+  @prop({ type: Number }) mass!: number;
+  @prop({ type: Number }) damage!: number;
+}
+
+@plugin(AutoIncrementID)
+@modelOptions({ schemaOptions: { collection: "jobs", versionKey: false } })
+@index({ id: 1 }, { unique: true })
+@index({ "driver.steam_id": 1 })
+@index({ start_timestamp: 1 })
+@index({ stop_timestamp: 1 })
+@index({ driven_distance: 1 })
+@index({ fuel_used: 1 })
+class JobSchema {
+  public _id!: number;
+  @prop({ type: () => SourceSchema, default: {} }) source!: SourceSchema;
+  @prop({ type: () => DriverSchema, default: {} }) driver!: DriverSchema;
+  @prop({ type: Number }) start_timestamp!: number;
+  @prop({ type: Number }) stop_timestamp!: number;
+  @prop({ type: Number }) driven_distance!: number;
+  @prop({ type: Number }) fuel_used!: number;
+  @prop({ type: () => CargoSchema, default: {} }) cargo!: CargoSchema;
+  @prop({ type: String }) source_city!: string;
+  @prop({ type: String }) source_company!: string;
+  @prop({ type: String }) destination_city!: string;
+  @prop({ type: String }) destination_company!: string;
+  @prop({ type: String }) truck!: string;
+  @prop({ type: Number }) averate_speed!: number;
+  @prop({ type: Number }) top_speed!: number;
+}
+
+export type JobDocument = DocumentType<JobSchema>;
+
+export const Jobs = getModelForClass(JobSchema);
