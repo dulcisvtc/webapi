6da2be66899aa4036e3a80365304e9505753c8f1
djoh <djoh@proxied.host>
2023-12-22 14:05:52 +0200

    feat(UserSchema): setup indexes

--- src/database/models/User.ts
+++ src/database/models/User.ts
@@ -1,4 +1,4 @@
-import { DocumentType, getModelForClass, modelOptions, prop, Severity } from "@typegoose/typegoose";
+import { DocumentType, getModelForClass, index, modelOptions, prop, Severity } from "@typegoose/typegoose";
 
 @modelOptions({ schemaOptions: { _id: false }, options: { allowMixed: Severity.ALLOW } })
 class LeaderboardSchema {
@@ -7,10 +7,16 @@ class LeaderboardSchema {
 }
 
 @modelOptions({ schemaOptions: { collection: "users" }, options: { allowMixed: Severity.ALLOW } })
+@index({ steam_id: 1 }, { unique: true })
+@index({ discord_id: 1 }, { unique: true })
+@index({ username: 1 }, { unique: true })
+@index({ steam_id: "text", discord_id: "text", username: "text" })
+@index({ "leaderboard.monthly_mileage": -1 })
+@index({ "leaderboard.alltime_mileage": -1 })
 export class UserSchema {
   @prop({ type: String, unique: true, required: true }) steam_id!: string;
   @prop({ type: String, unique: true, required: true }) discord_id!: string;
-  @prop({ type: String, required: true }) username!: string;
+  @prop({ type: String, unique: true, required: true }) username!: string;
   @prop({ type: Number, default: 0 }) permissions!: number;
   @prop({ type: Number, default: 0 }) experience!: number;
   @prop({ type: LeaderboardSchema, default: {} }) leaderboard!: LeaderboardSchema;
