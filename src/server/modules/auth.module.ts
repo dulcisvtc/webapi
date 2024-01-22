68348f3bbc581510ed51e39d7100dcf00f24ae22
djoh <djoh@proxied.host>
2024-01-23 00:32:26 +0200

    fix2

--- src/server/modules/auth.module.ts
+++ src/server/modules/auth.module.ts
@@ -3,8 +3,10 @@ import { AuthController } from "../controllers/auth.controller";
 import { PermissionsGuard } from "../guards/auth.guard";
 import { AuthService } from "../services/auth.service";
 import { UsersService } from "../services/users.service";
+import { UsersModule } from "./users.module";
 
 @Module({
+  imports: [UsersModule],
   controllers: [AuthController],
   providers: [AuthService, PermissionsGuard, UsersService],
   exports: [AuthService, PermissionsGuard],
