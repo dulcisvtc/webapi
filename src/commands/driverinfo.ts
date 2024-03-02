c486c6cd00b80943111a099f8c51cf6832f0e26d
djoh <djoh@proxied.host>
2024-03-02 22:52:19 +0100

    fix: monthly mileage calculation in driverinfo

--- src/commands/driverinfo.ts
+++ src/commands/driverinfo.ts
@@ -24,7 +24,17 @@ export default {
       .has(PermissionFlagsBits.SendMessages);
     await interaction.deferReply({ ephemeral });
 
-    const mdist = Math.round(document.leaderboard.monthly_mileage);
+    const monthTimestamp = Date.UTC(new Date().getUTCFullYear(), new Date().getUTCMonth());
+
+    const mdist = await Jobs.aggregate<{ mileage: number }>([
+      {
+        $match: {
+          "driver.steam_id": document.steam_id,
+          stop_timestamp: { $gte: monthTimestamp },
+        },
+      },
+      { $group: { _id: null, mileage: { $sum: "$driven_distance" } } },
+    ]).then((d) => d[0]?.mileage || 0);
     const adist = Math.round(document.leaderboard.alltime_mileage);
     const jobs = await Jobs.find({ "driver.steam_id": document.steam_id }).countDocuments();
     const player = await getTMPPlayer(document.steam_id).catch(() => null);
